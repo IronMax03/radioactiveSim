c4d53dd7dcda3ccc1435abc52fed43b6106c6739
maximilien_notz <146889648+IronMax03@users.noreply.github.com>
2026-03-13 09:11:37 +0100

    add some tests

--- src/test.cpp
+++ src/test.cpp
@@ -2,11 +2,13 @@
 #include <cassert>
 #include "Particle.h"
 #include "ROI.h"
+#include "Output.h"
+#include "Quadtree.h"
 
 using namespace std;
 
 int main()
-{
+{   
     // particules
     Particle p(0,0,0,1,0,0, "proton ");
 
@@ -22,4 +24,37 @@ int main()
     ROI temp(0, 1, 0, -1, 0, 1);
     assert(temp.contains(Particle(0.5, -0.5, 0.5,0,0,0, "positron")));
 
+
+    // write_particles_csv
+    write_particles_csv(vector<Particle>{Particle(0.01,0,0,1,0,0, "proton "), Particle(1,0,0,1,0,0, "proton ")}, "OUTPUT.csv");
+
+    // LazyNode
+    LazyNode<int> n;
+    n.value = 4;
+    assert(n.read() == n.value);
+    assert(!(n.is_eval_00() || n.is_eval_01() || n.is_eval_10() || n.is_eval_11()));
+
+    n.get_00().value = 0;
+    assert(n.is_eval_00());
+    assert(n.get_00().read() == 0);
+
+    n.get_01().value = 1;
+    assert(n.is_eval_01());
+    assert(n.get_01().read() == 1);
+
+    n.get_10().value = 2;
+    assert(n.is_eval_10());
+    assert(n.get_10().read() == 2);
+
+    n.get_11().value = 3;
+    assert(n.is_eval_11());
+    assert(n.get_11().read() == 3);
+
+    n.get_11().get_00().value = -1;
+    assert(n.get_11().get_00().read() == -1);
+
+
+    // quadtree
+    Quadtree q;
+    q.get_root().value = 3;
 }
