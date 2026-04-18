5daad7c8017e5d73f5ee59a54903d7417e7d6ac4
maximilien_notz <146889648+IronMax03@users.noreply.github.com>
2026-04-18 17:51:17 +0200

    adding edge case error handling in move() and docu

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,3 +1,7 @@
+/**
+ * @file Particle.cpp
+ * @brief This file contains the implementation of the Particle class methods.
+ */
 #include "Particle.h"
 
 Particle::Particle(double x_, double y_, double z_,
@@ -29,7 +33,15 @@ Particle::Particle(double x_, double y_, double z_,
       alive(true)
       {}
 
+/// @brief 
+/// @param dt 
+/// @throws std::runtime_error if dt is negative or zero.
+/// @note this error can be ignored whith a try catch if the final user wants to use a negative or zero time step.
+/// @note This error is mainly here to prevent the user from accidentally using a negative or zero time step, which would lead to undefined behavior in the simulation.
 void Particle::move(double dt) 
-{
+{    
     position += velocity * dt;
+
+    if (dt <= 0)
+        throw std::runtime_error("Particle movement error: Time step must be positive and not equal to zero.");
 }
